5bcf0ed2f0fd0fdd810172feadb006b5fcd2a337
Tom Tate <tomtate101@yahoo.com>
2025-05-07 13:55:57 -0700

    add hover eventListener to each box

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const MAX_HEIGHT = 16;
 const MAX_WIDTH = 16;
-
 const divContainer = document.querySelector(".container");
 
-
 createGrid(MAX_HEIGHT, MAX_WIDTH);
+
+const rows = document.querySelectorAll(".rowDiv");
+// const column = document.querySelectorAll(".columnDiv");
+
+rows.forEach((row) => {
+    const boxes = row.querySelectorAll(".columnDiv");
+    boxes.forEach((box) => {
+        box.addEventListener("mouseover", handleHover);
+    })
+})
 
 function createGrid(height, width){
     for (let i = 0; i < MAX_WIDTH; i++){
@@ -17,4 +25,9 @@ function createGrid(height, width){
             rowDiv.appendChild(columnDiv);
         }
     }
+}
+
+function handleHover(event){
+     event.target.style.backgroundColor = "orange";
+    
 }
