068ba95cabf20e1f7c006281a3d90ec26be3f0f5
Tom Tate <tomtate101@yahoo.com>
2025-05-07 23:36:27 -0700

    update slider text

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 const MAX_HEIGHT = 32;
 const MAX_WIDTH = 32;
-const divContainer = document.querySelector(".grid-container");
 
-createGrid(MAX_HEIGHT, MAX_WIDTH);
+createGrid(MAX_WIDTH, MAX_HEIGHT);
+addHoverListeners();
 
-const rows = document.querySelectorAll(".rowDiv");
-// const column = document.querySelectorAll(".columnDiv");
+const clearButton = document.querySelector("#clear-board")
+const gridSlider = document.querySelector("#grid-slider");
 
-rows.forEach((row) => {
-    const boxes = row.querySelectorAll(".columnDiv");
-    boxes.forEach((box) => {
-        box.addEventListener("mouseover", handleHover);
-    })
+clearButton.addEventListener("click", () => {
+    resetBoard();
 })
 
-function createGrid(height, width){
-    for (let i = 0; i < MAX_WIDTH; i++){
+gridSlider.addEventListener("input", () => {
+    const inputWidth = gridSlider.value;
+    const inputHeight = inputWidth;
+    const gridSizeText = document.querySelector(".grid-size-text");
+    gridSizeText.textContent = `Grid Size: ${inputWidth} x ${inputHeight}`;
+    clearGrid();
+    createGrid(inputWidth, inputHeight);
+    addHoverListeners();
+})
+
+function createGrid(width, height){
+    const divContainer = document.querySelector(".grid-container");
+
+    for (let i = 0; i < width; i++){
         const rowDiv = document.createElement("div");
         rowDiv.className = "rowDiv";
         divContainer.appendChild(rowDiv);
-        for (let j = 0; j < MAX_HEIGHT; j++){
+        for (let j = 0; j < height; j++){
             const columnDiv = document.createElement("div");
             columnDiv.className = "columnDiv";
             rowDiv.appendChild(columnDiv);
@@ -27,7 +36,38 @@ function createGrid(height, width){
     }
 }
 
+function clearGrid(){
+    const rows = document.querySelectorAll(".rowDiv");
+    rows.forEach((row) => {
+        const rowBoxes = row.querySelectorAll(".columnDiv");
+        rowBoxes.forEach((box) => {
+            box.remove();
+        })
+        row.remove();
+    })
+}
+
 function handleHover(event){
-     event.target.style.backgroundColor = "orange";
+     event.target.style.backgroundColor = "black";
     
+}
+
+function addHoverListeners(){
+    const rows = document.querySelectorAll(".rowDiv");
+    rows.forEach((row) => {
+        const boxes = row.querySelectorAll(".columnDiv");
+        boxes.forEach((box) => {
+            box.addEventListener("mouseover", handleHover);
+        })
+    })
+}
+
+function resetBoard(){
+    const rows = document.querySelectorAll(".rowDiv");
+    rows.forEach((row) => {
+        const boxes = row.querySelectorAll(".columnDiv");
+        boxes.forEach((box) => {
+            box.style.backgroundColor = "white";
+        })
+    })
 }
